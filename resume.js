963aef56f18d06e0d10e2fa85145becb2977aed6
Isaac-Amano <119136855+Isaac-Amano@users.noreply.github.com>
2024-04-29 21:03:57 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,3 +1,6 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-})
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('theme-toggle');
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme');
+    });
+});
